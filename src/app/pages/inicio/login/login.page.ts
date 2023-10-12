9c4d67d02025c86e3a4e5ae47c0da256973aefa5
elCapoMaximo <Masterhaqer324@gmail.com>
2023-10-12 01:55:02 -0300

    implementacion de errores

--- src/app/pages/inicio/login/login.page.ts
+++ src/app/pages/inicio/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-login',
@@ -16,18 +17,35 @@ export class LoginPage implements OnInit {
   })
 
   firebaseSvs = inject(FirebaseService);
+  utilsSvs = inject(UtilsService);
 
   ngOnInit() {
   }
 
-  submit(){
-    if(this.form.valid){
+  async submit(){
+    if (this.form.valid){
+
+      const loading = await this.utilsSvs.loading();
+      await loading.present();
+
       this.firebaseSvs.signIn(this.form.value as User).then(res => {
 
         console.log(res);
 
-      })
+      }).catch(error => {
+        console.log(error);
 
+        this.utilsSvs.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+      }).finally(() => {
+        loading.dismiss();
+      })
     }
   }
 }
